a0b1707faf06949b90b99689672ac08ce149f4fc
fhemery <fhemery@hemit.fr>
2021-11-08 22:50:32 +0100

    Update env variable

--- apps/referential/src/environments/environment.prod.ts
+++ apps/referential/src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  mongoUrl:
-    'mongodb+srv://ynov:ynov2021@fhemery.7sqpm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  mongoUrl: process.env.MONGO_URL,
 };
