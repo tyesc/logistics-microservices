4689f086a319d480a839cf6d7efb4f1c6b3592f4
Michaela <michell.alfoldiova@gmail.com>
2022-03-24 17:04:11 +0100

    commented product controller

--- apps/shopping/src/app/product/product.controller.ts
+++ apps/shopping/src/app/product/product.controller.ts
@@ -4,26 +4,26 @@ import * as contracts from '@log/contracts';
 
 @Controller('product')
 export class ProductController {
-  @Get()
-  async getAvailableProducts(): Promise<contracts.AvailableProductDto> {
-    const products = await axios.get('https://fhemery-logistics.herokuapp.com/api/products');
-    const stock = await axios.get('https://archi-logicielle.herokuapp.com/api/stock');
-
-    let availableProducts: as AvailableProductDto[]
-
-    products.forEach(product => {
-      let productStock = stock.find(el => el._id === product._id)
-      if(productStock.quantity > 0)
-      {
-        availableProducts.push({
-          id: product._id,
-          name: product.name,
-          description: product.description,
-          unitPrice: product.price
-        })
-      }
-    });
-
-    return availableProducts;
-  }
+  // @Get()
+  // async getAvailableProducts(): Promise<contracts.AvailableProductDto> {
+  //   const products = await axios.get('https://fhemery-logistics.herokuapp.com/api/products');
+  //   const stock = await axios.get('https://archi-logicielle.herokuapp.com/api/stock');
+  //
+  //   let availableProducts: as AvailableProductDto[]
+  //
+  //   products.forEach(product => {
+  //     let productStock = stock.find(el => el._id === product._id)
+  //     if(productStock.quantity > 0)
+  //     {
+  //       availableProducts.push({
+  //         id: product._id,
+  //         name: product.name,
+  //         description: product.description,
+  //         unitPrice: product.price
+  //       })
+  //     }
+  //   });
+  //
+  //   return availableProducts;
+  // }
 }
