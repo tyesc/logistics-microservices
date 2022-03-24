a32021a93032e45fc114a51635b68be232153dcb
tyesc <escolano.tyler@gmail.com>
2022-03-24 16:16:31 +0100

    feat: can get & add prouct
    Co-authored-by: Armelle.b <CaramelleB@users.noreply.github.com>

--- apps/shopping/src/app/basket/basket.controller.ts
+++ apps/shopping/src/app/basket/basket.controller.ts
@@ -1,9 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Put, HttpCode, NotFoundException } from '@nestjs/common';
+import axios from 'axios';
+import * as contracts from '@log/contracts';*
+import BasketRepository from '../repository/basket-repository';
+
+const basketRepository = new BasketRepository();
 
 @Controller('basket')
 export class BasketController {
   @Get()
-  public basket(): string {
-    return 'Basket';
+  async basket(): Promise<contracts.BasketDto> {
+    const basket = await basketRepository.getBasket();
+
+    return basket;
+  }
+
+  @Put()
+  @HttpCode(204)
+  async udpateBasket(@Body() addToBasketDto: contracts.AddToBasketDto): Promise<contracts.BasketDto> {
+    const basketDto = addToBasketDto
+    const product = await axios.get(`https://fhemery-logistics.herokuapp.com/api/products/${basketDto.id}`);
+
+    if(!product) {
+      throw new NotFoundException();
+    }
+
+    const basket = basketRepository.addProduct(product.data);
+
+    return basket
   }
 }
---
+++ apps/shopping/src/app/repository/basket-repository.ts
@@ -0,0 +1,23 @@
+import * as contracts from '@log/contracts';
+
+export default class BasketRepository {
+  private basket = new Map<string, contracts.BasketDto>();
+  private key = 'basket';
+
+  constructor() {
+    this.basket.set(this.key, { totalPrice: 0, products: [] });
+  }
+
+  public getBasket(): contracts.BasketDto {
+    return this.basket.get(this.key);
+  }
+
+  public addProduct(productDTO: contracts.ProductsAddedToBasketDto): contracts.BasketDto {
+
+    const basket = this.basket.get(this.key);
+    basket.products.push(productDTO);
+
+    return basket;
+  }
+
+}
